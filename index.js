e6a2a438d52960fc5415f352100a40c7df0d6c3d
Ryan Florence <rpflorence@gmail.com>
2015-01-05 10:37:17 -0700

    WOO!
    not the best code I've ever written, but I'm
    fasting, two of my kids made their way into my
    bed last night, and I haven't showered yet,
    so that's what you get.
    
    However, IT WORKS, so idc how ugly it is.

---
+++ index.js
@@ -0,0 +1,262 @@
+var router = require('nested-router');
+var pathToRegexp = require('path-to-regexp');
+var { resolve } = require('path-browserify');
+
+var cannotPush = (obj, path) => {
+  error(true, `Can't push to "${path}". Use "set"`);
+};
+
+var isTypeof = (desiredType) => {
+  return {
+    validate (val, path) {
+      var actualType = typeof val;
+      error(actualType !== desiredType, `Validation Error: Invalid value, expected "${desiredType}" but got "${actualType}" for path "${path}".`);
+    }
+  };
+};
+
+var toArray$ = (obj) => {
+  return Object.keys(obj).reduce((arr, key) => {
+    obj[key]._id = key;
+    arr.push(obj[key]);
+    return arr;
+  }, []);
+};
+
+var list = {
+  validate (val, path) {
+    error(true, 'Cannot call `set` on a `list`, use `ref.push`. Path '+path);
+  },
+
+  transform (snapshotVal) {
+    return toArray$(snapshotVal);
+  },
+
+  toString () {
+    return 'list';
+  }
+};
+
+var hash = {
+  validate (obj, path) {
+    error('object' !== typeof obj, `Validation Error: received non-object for path "${path}"`);
+  }
+};
+
+var index = (path) => {
+  return {
+    toString() {
+      return 'index';
+    },
+
+    getPath () {
+      return path;
+    }
+  }
+};
+
+var key = (path) => {
+  return {
+    toString () {
+      return 'key';
+    },
+
+    validate (val) {
+      error(typeof val !== 'string', `Validation Error: keys must be strings, got "${typeof val}"`);
+    },
+
+    getPath () {
+      return path;
+    }
+  }
+};
+
+var createRefForReals = (Firebase, path, routes, host) => {
+  var matchInfo = router.match(path, routes);
+  error(matchInfo === null, `The path "${path}" was not found in your schema`);
+  var handler = getLast(matchInfo.handlers);
+  var firebaseRef = new Firebase(`${host}/${path}`);
+  var set = (val, cb) => {
+    handler.validate(val, path);
+    if (handler === hash)
+      validateHash(val, path, routes, matchInfo);
+    firebaseRef.set(val, cb);
+  };
+  var push = (obj, cb) => {
+    validatePush(obj, path, routes, matchInfo);
+    firebaseRef.push(obj, cb);
+  };
+  var parse = (snapshot, cb) => {
+    var snapshotVal = snapshot.val();
+    var transformed = handler.transform ? handler.transform(snapshotVal) : snapshotVal;
+    addIdForLists(snapshotVal, matchInfo.route, path);
+    addRelationships(transformed, matchInfo);
+    addIndexes(transformed, matchInfo);
+    cb(null, transformed);
+  };
+  var getValue = (cb) => {
+    firebaseRef.once('value', (snapshot) => {
+      parse(snapshot, cb);
+    });
+  };
+  var child = (childPath) => {
+    return createRefForReals(Firebase, `${path}/${childPath}`, routes, host);
+  };
+  var listen = (userHandler) => {
+    var handler = snapshot => parse(snapshot, userHandler);
+    firebaseRef.on('value', handler);
+    return {
+      dispose () {
+        firebaseRef.off('value', handler);
+      }
+    };
+  };
+  var removeChangeListener = () => {
+  };
+  return { set:set, getValue, push, child, listen };
+};
+
+var addIdForLists = (val, route, path) => {
+  if (route.parent && route.parent.handler == 'list')
+    val._id = path.split('/').reverse()[0];
+};
+
+var error = (shouldThrow, message) => {
+  if (shouldThrow)
+    throw new Error(`FirebaseSchema Error: ${message}`);
+};
+
+var getLast = (arr) => {
+  return arr[arr.length - 1];
+};
+
+var validatePush = (obj, path, routes, matchInfo) => {
+  error(getLast(matchInfo.handlers) !== list, `Can't push to "${path}", use the "list" type.`);
+  Object.keys(obj).forEach((key) => {
+    var childPath = `${path}/:id/${key}`;
+    validateChild(obj[key], childPath, routes, matchInfo);
+  });
+};
+
+var validateHash = (obj, path, routes, matchInfo) => {
+  Object.keys(obj).forEach((key) => {
+    var childPath = `${path}/${key}`;
+    validateChild(obj[key], childPath, routes, matchInfo);
+  });
+};
+
+var validateChild = (val, path, routes, matchInfo) => {
+  var matchInfo = router.match(path, routes);
+  error(matchInfo === null, `No child path defined at "${path}"`);
+  var handler = getLast(matchInfo.handlers);
+  handler.validate(val, path);
+};
+
+var addIndexes = (val, matchInfo, depth) => {
+  depth = depth === undefined ? 0 : depth;
+  if (Array.isArray(val)) {
+    val.forEach(v => addIndexes(v, matchInfo, depth + 1));
+    return;
+  }
+  var path = matchInfo.path;
+  var dependencies = [];
+  recurseRoutes(matchInfo.route.children, (route) => {
+    if (route.handler == 'index') {
+      var relativePath = shrinkPath(route.handler.getPath(), depth);
+      var refPath = resolve('/', path, relativePath).substr(1);
+      var valueKey = route.path.replace(route.parent.path, '').substr(1);
+      var childRoute = route.children[0];
+      var childKey = childRoute.path.replace(childRoute.parent.path, '').substr(2)
+      if (!val[valueKey]) // nothing in the index yet
+        return;
+      var relationships = Object.keys(val[valueKey]).reduce((relationships, key) => {
+        relationships.push(`${refPath}/${key}`);
+        return relationships;
+      }, []);
+      val._indexes = val._indexes || {};
+      val._indexes[valueKey] = relationships;
+    }
+  });
+};
+
+var shrinkPath = (path, depth) => {
+  var c = depth;
+  while (c > 0) {
+    path = path.replace(/^..\//, '');
+    c--;
+  }
+  return path;
+};
+
+var addRelationships = (val, matchInfo, depth) => {
+  // sometimes I know what I'm doing, other times there's this.
+  // Might have some recursion issues? My data isn't that nested
+  // so I don't know/don't care ... yet.
+
+  depth = depth === undefined ? 0 : depth;
+  if (Array.isArray(val)) {
+    val.forEach((v) => addRelationships(v, matchInfo, depth + 1));
+    return;
+  }
+  var path = matchInfo.path;
+  var dependencies = []; // terrible name
+  recurseRoutes(matchInfo.route.children, (route) => {
+    if (route.handler == 'key') {
+      var relativePath = shrinkPath(route.handler.getPath(), depth);
+      dependencies.push({
+        refPath: resolve('/', path, relativePath).substr(1),
+        valueKey: route.path.replace(route.parent.path, '').substr(1)
+      })
+    }
+  });
+  if (dependencies.length === 0)
+    return
+  var params = dependencies.reduce((params, dependency) => {
+    var keys = [];
+    pathToRegexp(dependency.refPath, keys);
+    keys.forEach((key) => {
+      params[key.name] = val[dependency.valueKey];
+    });
+    return params;
+  }, {});
+  var links = dependencies.reduce((links, dependency) => {
+    links[dependency.valueKey] = replaceParams(dependency.refPath, params);
+    return links;
+  }, {});
+  Object.keys(params).forEach((param) => {
+    if (params[param] === undefined)
+      console.warn(`Relationship Error: missing key "${param}" at path "${path}":`, val);
+  });
+  val._links = links;
+};
+
+var replaceParams = (path, params) => {
+  for (var key in params)
+    path = path.replace(':'+key, params[key]);
+  return path;
+};
+
+var recurseRoutes = (routes, iterate) => {
+  routes.forEach((route) => {
+    iterate(route);
+    if (route.children)
+      recurseRoutes(route.children, iterate);
+  });
+};
+
+exports.create = (Firebase, host, getRoutes) => {
+  var routes = router.map(getRoutes);
+  var createRef = path => createRefForReals(Firebase, path, routes, host);
+  return { createRef };
+};
+
+exports.Types = {
+  string: isTypeof('string'),
+  number: isTypeof('number'),
+  boolean: isTypeof('boolean'),
+  list,
+  hash,
+  index,
+  key
+};
+
---
+++ tests.js
@@ -0,0 +1,350 @@
+var expect = require('expect');
+var async = require('async');
+var Firebase = require('firebase');
+var Schema = require('./index');
+var { string, number, boolean, list, hash, index, key } = Schema.Types;
+console.log(Date.now());
+
+var _warn = console.warn;
+var consoleWarnings = [];
+console.warn = function () {
+  consoleWarnings.push([].slice.call(arguments, 0));
+};
+
+var HOST = 'http://rf-testing.firebaseio.com';
+var testRef = new Firebase(`${HOST}/test`);
+
+beforeEach((done) => {
+  consoleWarnings = [];
+  testRef.remove(() => done())
+});
+
+describe('schema', () => {
+  it('complains if a url does not match', () => {
+    var schema = Schema.create(Firebase, HOST, (child) => {
+      child('test', string);
+    });
+    expect(() => {
+      schema.createRef('not-defined')
+    }).toThrow(/not found/);
+  });
+});
+
+describe('ref', () => {
+  it('sets and reads from a ref', (done) => {
+    var schema = Schema.create(Firebase, HOST, (child) => {
+      child('test', string);
+    });
+    var ref = schema.createRef('test');
+    ref.set('foo');
+    ref.getValue((err, val) => {
+      expect(val).toEqual('foo');
+      done();
+    });
+  });
+
+  describe('listen', () => {
+    it('listens', (done) => {
+      var schema = Schema.create(Firebase, HOST, (child) => {
+        child('test', string);
+      });
+      var ref = schema.createRef('test');
+      var listener = ref.listen((err, val) => {
+        expect(val).toEqual('foo');
+      });
+      ref.set('foo', () => {
+        listener.dispose();
+        done();
+      });
+    });
+
+    it('stops listening', (done) => {
+      var schema = Schema.create(Firebase, HOST, (child) => {
+        child('test', string);
+      });
+      var ref = schema.createRef('test');
+      var listenerCalls = 0
+      var listener = ref.listen((err, val) => {
+        listenerCalls++;
+        if (listenerCalls === 1)
+          expect(val).toEqual('foo');
+        else
+          throw new Error('should not have gone this far');
+      });
+      ref.set('foo', () => {
+        listener.dispose();
+        ref.set('bar', () => done());
+      });
+    });
+  });
+
+  describe('set', () => {
+    it('validates strings', () => {
+      var schema = Schema.create(Firebase, HOST, (child) => {
+        child('test', string);
+      });
+      var ref = schema.createRef('test');
+      expect(() => ref.set(123)).toThrow(/expected "string" but got "number"/i);
+    });
+
+    it('validates numbers', () => {
+      var schema = Schema.create(Firebase, HOST, (child) => {
+        child('test', number);
+      });
+      var ref = schema.createRef('test');
+      expect(() => ref.set('abc')).toThrow(/expected "number" but got "string"/i);
+    });
+
+    describe('hash', () => {
+      it('sets a hash', (done) => {
+        var schema = Schema.create(Firebase, HOST, (child) => {
+          child('test/:id', hash, (child) => {
+            child('name', string);
+          });
+        });
+        var ref = schema.createRef('test/ryan');
+        ref.set({name: 'Ryan Florence'});
+        ref.getValue((err, val) => {
+          expect(val).toEqual({name: 'Ryan Florence'});
+          done();
+        });
+      });
+
+      it('validates values', () => {
+        var schema = Schema.create(Firebase, HOST, (child) => {
+          child('test/:id', hash, (child) => {
+            child('name', string);
+          });
+        });
+        var ref = schema.createRef('test/ryan');
+        expect(() => ref.set({name: 123})).toThrow(/expected "string" but got "number"/i);
+      });
+    });
+  });
+
+  describe('push', () => {
+    describe('list', () => {
+      it('pushes nodes to lists', (done) => {
+        //ref.set; should throw!
+        // need limits
+        var schema = Schema.create(Firebase, HOST, (child) => {
+          child('test', list, (child) => {
+            child(':id', hash, (child) => {
+              child('name', string);
+            });
+          });
+        });
+        var ref = schema.createRef('test');
+        ref.push({ name: 'ryan' });
+        ref.getValue((err, val) => {
+          var { _id } = val[0];
+          expect(val).toEqual([
+            { _id, name: 'ryan' }
+          ]);
+          done();
+        });
+      });
+
+      it('validates the pushed object values', () => {
+        var schema = Schema.create(Firebase, HOST, (child) => {
+          child('test', list, (child) => {
+            child(':id', hash, (child) => {
+              child('name', string);
+            });
+          });
+        });
+        var ref = schema.createRef('test');
+        expect(() => ref.push({name: 123})).toThrow(/expected "string" but got "number"/i);
+      });
+
+      it('throws if you try to push to a non-list path', () => {
+        var schema = Schema.create(Firebase, HOST, (child) => {
+          child('test', string);
+        });
+        var ref = schema.createRef('test');
+        expect(() => {
+          ref.push({})
+        }).toThrow(/can't push to \"test\"/i);
+      });
+
+      it('throws if you try to push an object with the wrong keys', () => {
+        var schema = Schema.create(Firebase, HOST, (child) => {
+          child('test', list, (child) => {
+            child('name');
+          });
+        });
+        var ref = schema.createRef('test');
+        expect(() => {
+          ref.push({name: 'ryan', age: 33});
+        }).toThrow(/no child path defined/i);
+      });
+    });
+  });
+});
+
+describe('relationships', () => {
+
+  var usersRef;
+  var groupsRef;
+  var messagesRef;
+  var flagsRef;
+  var userId;
+  var groupId;
+  var messageId;
+  var flagId;
+
+  var schema = Schema.create(Firebase, HOST, (child) => {
+    child('test', hash, (child) => {
+      child('users', list, (child) => {
+        child(':userId', hash, (child) => {
+          child('name', string);
+          child('groups', index('../../groups'), (child) => {
+            child(':groupId', boolean);
+          });
+        });
+      });
+
+      child('groups', list, (child) => {
+        child(':groupId', hash, (child) => {
+          child('name', string);
+          child('members', index('../../users'), (child) => {
+            child(':userId', boolean)
+          });
+        });
+      });
+
+      child('messages/:groupId', list, (child) => {
+        child(':messageId', hash, (child) => {
+          child('content', string);
+          child('author', key('../../users/:userId'))
+        });
+      });
+
+      child('messageFlags', list, (child) => {
+        child(':flagId', hash, (child) => {
+          child('message', key('../../messages/:groupId/:messageId'));
+          child('group', key('../../groups/:groupId'));
+          child('count', number);
+        });
+      });
+    });
+  });
+
+  beforeEach((done) => {
+    usersRef = schema.createRef('test/users');
+    groupsRef = schema.createRef('test/groups');
+    async.parallel([
+      (cb) => usersRef.push({name: 'ryan'}, cb),
+      (cb) => groupsRef.push({name: 'cool kids'}, cb)
+    ], () => {
+      async.parallel({
+        users (cb) { usersRef.getValue(cb) },
+        groups (cb) { groupsRef.getValue(cb) }
+      }, (err, results) => {
+        userId = results.users[0]._id;
+        groupId = results.groups[0]._id;
+        async.parallel({
+          users (cb) { usersRef.child(`${userId}/groups/${groupId}`).set(true, cb) },
+          groups (cb) { groupsRef.child(`${groupId}/members/${userId}`).set(true, cb) },
+          messages (cb) {
+            messagesRef = schema.createRef(`test/messages/${groupId}`);
+            messagesRef.push({ content: 'hello', author: userId }, (err) => {
+              messagesRef.getValue(cb);
+            });
+          }
+        }, (err, results) => {
+          messageId = results.messages[0]._id;
+          flagsRef = schema.createRef(`test/messageFlags`);
+          flagsRef.push({ count: 2, group: groupId, message:messageId }, () => {
+            flagsRef.getValue((err, flags) => {
+              // phew!
+              flagId = flags[0]._id;
+              done();
+            });
+          });
+        })
+      });
+    });
+  });
+
+  describe('key', () => {
+    it('adds links to list values', (done) => {
+      flagsRef.getValue((err, flags) => {
+        expect(flags[0]._links).toEqual({
+          message: `test/messages/${groupId}/${messageId}`,
+          group: `test/groups/${groupId}`,
+        });
+        done();
+      });
+    });
+
+    it('adds links values', (done) => {
+      flagsRef.child(flagId).getValue((err, flag) => {
+        var expected = {
+          count: 2,
+          group: groupId,
+          message: messageId,
+          _id: flagId,
+          _links: {
+            message: `test/messages/${groupId}/${messageId}`,
+            group: `test/groups/${groupId}`,
+          }
+        };
+        expect(flag._links).toEqual(expected._links);
+        done();
+      });
+    });
+
+    it("warns if it doesn't have all the keys it needs", (done) => {
+      flagsRef.push({
+        count: 1,
+        group: groupId
+      }, () => {
+        flagsRef.getValue((err, flags) => {
+          expect(consoleWarnings.length).toEqual(1);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('index', () => {
+    it('adds indexes to the value', (done) => {
+      usersRef.child(userId).getValue((err, user) => {
+        expect(user._indexes).toEqual({
+          groups: [`test/groups/${groupId}`]
+        });
+        done();
+      });
+    });
+
+    it('adds indexes to lists', (done) => {
+      usersRef.getValue((err, users) => {
+        expect(users[0]._indexes).toEqual({
+          groups: [`test/groups/${groupId}`]
+        });
+        done();
+      });
+    });
+
+  });
+});
+
+
+
+//var transactions = store.ref(
+  //`/users/${uid}/transactions/${monthId}`
+//).listen(this.forceUpdate.bind(this));
+//transactions.dispose();
+
+//store.push(`/users/${uid}/transactions/${monthid}`, tx);
+//store.set(`/users/${uid}/transactions/${monthid}/${txId}`, tx);
+
+//var user = schema.ref(`/users/${uid}`);
+//var txRef = user.ref(`/transactions/${monthid}`).
+  //connect(this.forceUpdate.bind(this));
+
+//var account = schema.ref(`/users/${uid}/accounts/${accountId}`);
+//account.remove(); // remove all foreignKey's too
+
+
